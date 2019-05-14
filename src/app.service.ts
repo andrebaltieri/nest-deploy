29a4c6af9d16ed0c18e36ba530dce9775958df59
Andre Baltieri <andrebaltieri@hotmail.com>
2019-05-14 10:44:13 -0300

    add env vars

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello from ' + process.env.NODE_ENV;
   }
 }
